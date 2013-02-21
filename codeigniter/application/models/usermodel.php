7f9e58c48ca32cba88bc29e2e4b1f65be6774ae5
Nobuki Yoda <nobuki.y@gmail.com>
2013-02-21 15:10:08 +0900

    書き直し。

--- codeigniter/application/models/usermodel.php
+++ codeigniter/application/models/usermodel.php
@@ -4,11 +4,11 @@ class UserModel extends CI_Model {
 
     const TABLE_NAME = 'users';
 
-    var $id = NULL;
-    var $email = '';
-    var $password_hash = '';
-    var $username = '';
-    var $register_date = '';
+    public $id = NULL;
+    public $email = '';
+    public $password_hash = '';
+    public $username = '';
+    public $register_date = '';
 
     function __construct()
     {
@@ -18,38 +18,51 @@ class UserModel extends CI_Model {
         // テーブルが無ければ作る。
         if (!$this->db->table_exists(self::TABLE_NAME))
         {
-            $this->load->dbforge();
-
-            $fields = array(
-                'id' => array('type' => 'INT', 'unsigned' => TRUE, 'auto_increment' => TRUE),
-                'email' => array('type' => 'VARCHAR', 'constraint' => '256'),
-                'username' => array('type' =>'VARCHAR', 'constraint' => '64'),
-                'password_hash' => array('type' => 'VARCHAR', 'constraint' => '64'),
-                'register_date' => array('type' => 'DATETIME')
-            );
-            $this->dbforge->add_field($fields);
-            $this->dbforge->add_key('id', TRUE);
-            $this->dbforge->create_table(self::TABLE_NAME);
+            $this->_create_table();
         }
     }
 
-    // 成功したらTRUEを返す
-    function add_user()
+    // テーブルを新規作成
+    private function _create_table()
     {
-        $this->email = $_POST['email'];
-        $this->username = $_POST['username'];
-        $this->password_hash = do_hash($_POST['password']);
+        $this->load->dbforge();
+
+        $fields = array(
+            'id' => array('type' => 'INT', 'unsigned' => TRUE, 'auto_increment' => TRUE),
+            'email' => array('type' => 'VARCHAR', 'constraint' => '256'),
+            'username' => array('type' =>'VARCHAR', 'constraint' => '64'),
+            'password_hash' => array('type' => 'VARCHAR', 'constraint' => '64'),
+            'register_date' => array('type' => 'DATETIME')
+        );
+        $this->dbforge->add_field($fields);
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->create_table(self::TABLE_NAME);
+    }
+
+    // ユーザーを追加してユーザ情報を返す
+    function add_user($email, $username, $password)
+    {
+        // emailアドレスがログインIDなので重複チェック
+        $count = $this->db->from(self::TABLE_NAME)->where('email', $email)->count_all_results();
+        if ($count > 0) {
+            throw new Exception("メールアドレスが既に使用されています。");
+        }
+ 
+        $this->email = $email;
+        $this->username = $username;
+        $this->password_hash = do_hash($password);
         $this->register_date = date("Y-m-d H:i:s", time());
 
         $this->db->insert(self::TABLE_NAME, $this);
         $this->id = $this->db->insert_id();
-        return TRUE;
+        return $this;
     }
 
-    function login()
+    // ログインしてユーザ情報を返す
+    // セキュリティ的にはpassword_hashとかも保存しとくべき？
+    function login($email, $password)
     {
-        $email = $_POST['email'];
-        $password_hash = do_hash($_POST['password']);
+        $password_hash = do_hash($password);
         $login_cond = array('email' => $email, 'password_hash' => $password_hash);
         $query = $this->db->get_where(self::TABLE_NAME, $login_cond);
 
@@ -59,15 +72,14 @@ class UserModel extends CI_Model {
             $user = $users[0];
             $login_data = array('user_id' => $user->id);
             $this->session->set_userdata($login_data);
-            return TRUE;
+            return $user;
         }
-        return FALSE;
+        throw new Exception("メールアドレスまたはパスワードが違います。");
     }
 
+    // ログアウト
     function logout()
     {
         $this->session->unset_userdata('user_id');
     }
 }
-
-?>
